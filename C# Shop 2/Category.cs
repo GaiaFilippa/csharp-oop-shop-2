5fdb2fbd264f273ac4252c0a4ff4f6bd129ac9c1
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-20 14:01:26 +0200

    Aggiungere i file di progetto.

---
+++ C# Shop 2/Category.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Shop_2
+{
+    internal class Category
+    {
+        private string name;
+        private string description;
+        private int code;
+
+        // COSTRUTTORE
+
+        public Category(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+            this.code = RandomNumber();
+        }
+        // GETTERS
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetDescription()
+        {
+            return this.description;
+        }
+
+        public int GetCode()
+        {
+            return this.code;
+        }
+
+        //METODI 
+
+        private int RandomNumber()
+        {
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(10000000);
+
+            return randomNumber;
+        }
+
+        public string GetCateogriesString()
+        {
+            string rapprString = "La categoria è: " + this.name + "\n";
+            rapprString += this.description + "\n";
+            rapprString += "Codice prodotto: " + this.code + "\n";
+            return rapprString;
+        }
+    }
+}
---
+++ C# Shop 2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// DEFINISCO IL MIO SHOP
+
+using C__Shop_2;
+
+Shop negozio = new Shop("Di tutto e di più", "Palermo", "Via Roma", 89, "Alimentare");
+
+// DEFINISCO I PRODOTTI CHE IL MIO SHOP TRATTA
+itemShop insalata = new itemShop("Insalata");
+itemShop pollo = new itemShop("Pollo");
+
+// DEFINISCO LE CATEGOIRE PER I MIEI PRODOTTO
+Category verdura = new Category("Verdura", "Insalata verde");
+Category carne = new Category("Carne", "Pollo allevato a terra");
+
+// ASSOCIO I PRODOTTI CON LE LORO CATEGORIE
+insalata.addCaetogy(verdura);
+pollo.addCaetogy(carne);
+
+negozio.addItem(insalata);
+negozio.addItem(pollo);
+
+
+Console.WriteLine(negozio.InfoShop());
